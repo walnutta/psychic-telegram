5427508065ad2f55bfc3850fe243bd7876248a7b
Nhi Nguyen <129512777+walnutta@users.noreply.github.com>
2025-11-03 01:44:48 +0000

    changes to everything

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,20 @@
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+  // Copy assets
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  // Add shortcode
+  eleventyConfig.addShortcode("reusableNote", function() {
+    return `<p>🧋 This site is powered by Eleventy.</p>`;
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
